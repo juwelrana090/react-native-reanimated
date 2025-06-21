4d396368fed57c3b72ea42cf2dde6dbd8163a499
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2025-06-22 04:38:19 +0600

    Reat Native Projact Setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
+import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import './global.css';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
